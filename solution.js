bb12b7bb80561e27e865b9891c9c7aaf2cd32eab
Alvaro Garcia <derecerca@gmail.com>
2015-01-25 00:30:06 +0100

    refactor: stop mid lift to pick up a passenger
    there's one elevator going up and another down

--- solution.js
+++ solution.js
@@ -1,5 +1,13 @@
 {
     init: function(elevators, floors) {
+        var myLog = function(text, elevator){
+            var hasElevator = elevator !== undefined;
+            if(hasElevator){
+                text = "["+elevator+"]: " + text;
+            }
+            console.log(text);
+        }
+
         
         elevators.forEach(function(current, index){
             current.on("floor_button_pressed", function(floorNum) { 
@@ -7,28 +15,31 @@
                 current.goToFloor(floorNum);
             });
             current.on("passing_floor", function(floorNum, direction) { 
-                console.log("passing_floor: " + floorNum + ", " + direction);
+                myLog("passing_floor: " + floorNum + ", " + direction+", load: "+ current.loadFactor(), index);
                 if(current.loadFactor() < 0.8){
                     if(direction === "up" && waitingAt_Up[floorNum] > 0){
-                        current.goToFloor(floorNum, true);
-                        waitingAt_Up[floorNum]--;
+                        myLog("entered mid lift - up", index);
+                        elevators[0].goToFloor(floorNum, true);
+                        waitingAt_Up[floorNum] = 0;
                     }
                     if(direction === "down" && waitingAt_Down[floorNum] > 0){
-                        current.goToFloor(floorNum, true);
-                        waitingAt_Down[floorNum]--;
+                        myLog("entered mid lift - down", index);
+                        elevators[1].goToFloor(floorNum, true);
+                        waitingAt_Down[floorNum] = 0;
                     }
                 }
             });
             current.on("idle", function(floorNum) { current.goToFloor(0); } );
         });
-        
+
         var waitingAt_Up= [];
         var waitingAt_Down= [];
         floors.forEach(function(_,index){
             waitingAt_Up.push(0);
             waitingAt_Down.push(0);
         });
-        
+
+
         floors.forEach(function(current, index){
             current.on("up_button_pressed", function() { waitingAt_Up[index]++; } );
             current.on("down_button_pressed", function() { waitingAt_Down[index]++; } );
